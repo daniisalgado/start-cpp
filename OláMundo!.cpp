49ba9ab93e933abd981f56c3f5f6e62cb1e32a88
Daniela Salgado <160171100+daniisalgado@users.noreply.github.com>
2025-11-30 14:07:22 -0400

    Add files via upload

---
+++ OláMundo!.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+/*
+Iostream é uma biblioteca
+I - Vem de Input - Entrada
+O - Vem de Output - Saída
+*/
+
+#include <locale.h> //Para usarmos acentos
+
+/*
+Importando todos os identificadores do NameSpace STD para usar o cout e endl
+O Namespace é nada mais nada menos que uma organização de pastas
+*/
+using namespace std;
+
+/*
+Todo o programa em C++ tem uma e somente uma função principal que é a função MAIN
+*/
+
+int main () { 
+      
+     setlocale(LC_ALL, "");
+     cout << "Olá, Mundo! \n"; 
+     
+     cout << "Eu sou a Daniela Salgado e vou dominar essa linguagem! " << endl;
+//A função main diz que retornar um inteiro, então colocamos o 0 como tipo retorno
+     return 0;
+}
